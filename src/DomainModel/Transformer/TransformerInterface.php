b50bcd4afa691dbad819dd8b3ceef42c2e639e47
Michael Zippe <michael.zippe@power.cloud>
2022-12-09 14:36:08 +0100

    TransformerInterface added

---
+++ src/DomainModel/Transformer/TransformerInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Powercloud\SRT\DomainModel\Transformer;
+
+use Powercloud\SRT\DomainModel\Input\ReportInterface;
+use Powercloud\SRT\DomainModel\Output\ExternalIssuesReport;
+
+interface TransformerInterface
+{
+    public function transform(ReportInterface $report): ExternalIssuesReport;
+}
