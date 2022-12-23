ce3ae374b341b794ab4ef8db977036d135be6b04
Michael Zippe <michael.zippe@power.cloud>
2022-12-23 13:38:28 +0100

    [WIP] Functional tests for commands added, finishing with errors

---
+++ tests/Functional/Command/TranslateDeptracReportCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Powercloud\SRT\Tests\Functional\Command;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TranslateDeptracReportCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('srt:translate:deptrac');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'path' => __DIR__ . '/../../TestFiles/deptrac.json',
+            'externalIssuesReportPath' => '/../../Output/Functional/ExternalIssuesReport.json'
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Username: Wouter', $output);
+    }
+}
---
+++ tests/Functional/Command/TranslatePhpmdReportCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Powercloud\SRT\Tests\Functional\Command;
+
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TranslatePhpmdReportCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('srt:translate:phpmd');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'path' => __DIR__ . '/../../TestFiles/phpmd.json',
+            'externalIssuesReportPath' => '/../../Output/Functional/ExternalIssuesReport.json'
+        ]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        $output = $commandTester->getDisplay();
+
+        $this->assertStringContainsString('Username: Wouter', $output);
+    }
+}
