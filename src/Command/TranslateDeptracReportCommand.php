62181321021d8406ba6b162f5b39f4d3d528d37c
Michael Zippe <michael.zippe@power.cloud>
2023-01-04 10:05:28 +0100

    Suppress PHPMD warnings for UnusedFormalParameter for method required by signature of interface

--- src/Command/TranslateDeptracReportCommand.php
+++ src/Command/TranslateDeptracReportCommand.php
@@ -7,7 +7,7 @@ namespace Powercloud\SRT\Command;
 use Powercloud\SRT\DomainModel\Input\DeptracReport;
 use Powercloud\SRT\DomainModel\Transformer\DeptracTransformer;
 use Powercloud\SRT\DomainModel\Transformer\TransformerOptions;
-use Symfony\Component\Console\Input\InputArgument;
+use Powercloud\SRT\Exception\UnsupportedReportForTransformer;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -28,6 +28,10 @@ class TranslateDeptracReportCommand extends AbstractTranslatorCommand
         $this->setName(name: 'srt:translate:deptrac');
     }
 
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @throws UnsupportedReportForTransformer
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $deptracFileContent = $this->getFileContent((string) $input->getArgument('path'));
